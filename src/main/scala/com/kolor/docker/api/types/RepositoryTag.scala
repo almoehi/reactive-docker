dbb421692685309bdb686bfeea3471b60333f449
almoehi <almoehi@gmail.com>
2014-03-24 16:53:59 +0100

    FIXED: allowing empty  "<none>:<none>" repository tags which may occur during images listing

--- src/main/scala/com/kolor/docker/api/types/RepositoryTag.scala
+++ src/main/scala/com/kolor/docker/api/types/RepositoryTag.scala
@@ -12,10 +12,12 @@ class RepositoryTag private[RepositoryTag] (val repo: String, val tag: Option[St
 
 object RepositoryTag {
 	  val pattern = """^(\w+):?(\w*)$""".r
+	  val patternNone = """^(<none>):?(<none>)*$""".r
 
 	  def apply(s: String): RepositoryTag = s match {
 	    case pattern(repo, tag: String) => new RepositoryTag(repo, Some(tag))
 	    case pattern(repo, _) => new RepositoryTag(repo, None)
+	    case patternNone(_, _) => new RepositoryTag("none", Some("none"))	// there might be images with no tags (e.g. zombie images)
 		case _ => throw InvalidRepositoryTagFormatException(s"$s is an invalid repository tag", s)
 	  }
 	  
